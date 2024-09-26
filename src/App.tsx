a2c51c832a191a766fb454e6deb31cb4304a5ffb
Juan Carlos Gomez Jaramillo <juangomez88@gmail.com>
2024-09-25 19:28:22 -0500

    Primer commit
    Se crea el  repositorio con sus archivos

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { menuItems } from "./data/db"
+
+function App() {
+
+  console.log(menuItems)
+
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">Calculadora de Propinas</h1>
+      </header>
+
+      <main className=" max-w-7xl mx-auto mt-20 py-20 grid md:grid-cols-2">
+        <div>
+          <h2>Menú</h2>
+        </div>
+        
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
+
+    </>
+  )
+}
+
+export default App
