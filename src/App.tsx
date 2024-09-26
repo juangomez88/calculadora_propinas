a250adf220ac8c68e3a932e516f9f0124f2f9750
Juan Carlos Gomez Jaramillo <juangomez88@gmail.com>
2024-09-25 22:10:03 -0500

    Añadir articulos

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import MenuItems from "./components/MenuItems"
 import { menuItems } from "./data/db"
+import useOrder from "./hooks/useOrder"
 
 function App() {
 
-  console.log(menuItems)
+  const { addItem } = useOrder()
 
   return (
     <>
@@ -11,10 +13,19 @@ function App() {
       </header>
 
       <main className=" max-w-7xl mx-auto mt-20 py-20 grid md:grid-cols-2">
-        <div>
-          <h2>Menú</h2>
+        <div className="p-5">
+          <h2 className=" text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map(item => (
+              <MenuItems
+                key={item.id}
+                item={item}
+                addItem={addItem}
+              />
+            ))}
+          </div>
         </div>
-        
+
         <div>
           <h2>Consumo</h2>
         </div>
---
+++ src/components/MenuItems.tsx
@@ -0,0 +1,19 @@
+import { MenuItem } from "../types"
+
+type MenuItemProps = {
+    item: MenuItem, 
+    addItem: () => void
+}
+
+
+export default function MenuItems({ item, addItem }: MenuItemProps) {
+    return (
+        <button
+            className=" border-2 border-teal-400 hover:bg-teal-200 w-full p-3 text-lg rounded-lg flex justify-between mb-5"
+            onClick={() => addItem()}
+        >
+            <p>{item.name}</p>
+            <p className=" font-black">${item.price}</p>
+        </button>
+    )
+}
